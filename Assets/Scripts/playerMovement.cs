b1a53592f4cab30bb8f9fa0e060311e1a2337202
kaii <karol.4444@proton.me>
2024-11-17 02:01:37 +0100

    booleans, finish later

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public Animator animator;
     public float horizontal;
     public float vertical;
     public Rigidbody2D rb;
-
     public float speed = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator.SetBool("isMoving", false);
+        animator.SetBool("facingUp", false);
+        animator.SetBool("facingDown", true);
+        animator.SetBool("facingLeft", false);
+        animator.SetBool("facingRight", false);
     }
 
     // Update is called once per frame
@@ -21,7 +26,48 @@ public class playerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+        if (vertical != 0)
+        {
+            //animator.SetBool("isMoving", true);
+            
+            if (vertical < 0)
+            {
+                animator.SetBool("facingDown", true);
+                animator.SetBool("facingUp", false);
+            }
+            else
+            {
+                animator.SetBool("facingDown", false);
+                animator.SetBool("facingUp", true);
+            }
+        }
+        else if (vertical == 0 && horizontal != 0)
+        {
+            //animator.SetBool("isMoving", true);
+            
+            animator.SetBool("facingUp", false);
+            animator.SetBool("facingDown", false);
+
+            if (horizontal < 0)
+            {
+                animator.SetBool("facingRight", false);
+                animator.SetBool("facingLeft", true);
+            }
+            else
+            {
+                animator.SetBool("facingRight", true);
+                animator.SetBool("facingLeft", false);
+            }
+        }
+
+        if (rb.linearVelocity != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 
     public void FixedUpdate()
